8c4cc60d4768d2306ef6403949380000eb6d9975
luyandra.branco <luyandra.branco@tis.ao>
2024-09-20 13:50:07 +0100

    refactor: elemento Task ajustado algumas melhorias

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,10 +1,15 @@
+import { FiTrash } from "react-icons/fi";
 import styles from "../styles/Task.module.scss";
+
 export function Task() {
   return (
     <div className={styles["task-container"]}>
-      <div>
+      <div className={styles["description-task"]}>
         <input type="checkbox" /> Lavar
       </div>
+      <button className={styles["btn-delete"]}>
+        <FiTrash/>
+      </button>
     </div>
   );
 }
