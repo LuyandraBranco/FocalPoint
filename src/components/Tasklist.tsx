35b4b5b6af10c795687ecae496e0730bbd0a116b
luyandra.branco <luyandra.branco@tis.ao>
2024-09-20 10:37:14 +0100

    feat: adicionadas áreas de tarefas por fazer e tarefas feitas

--- src/components/Tasklist.tsx
+++ src/components/Tasklist.tsx
@@ -4,7 +4,16 @@ export function Tasklist() {
   return (
     <section className={styles.tasklist}>
       <div className={styles["tasklist-elements-container"]}>
-        <Task />
+        <div className={styles["tasklist-todo"]}>
+          <p>Suas tarefas de hoje</p>
+          <Task />
+          <Task />
+          <Task />
+        </div>
+        <div className={styles["tasklist-done"]}>
+          <p>Tarefas finalizadas</p>
+          <Task />
+        </div>
       </div>
       <button type="submit" className={styles["button-create"]}>
         Adicionar nova tarefa
