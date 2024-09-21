2518c24ef841ad972dda3aa3c637a1f0de48f476
LuyandraBranco <luyandrabranco@gmail.com>
2024-09-21 21:37:40 +0100

    refactor: tipagem de encryptData e descrypt mudadas

--- src/utils/encryption.ts
+++ src/utils/encryption.ts
@@ -1,10 +1,11 @@
-export function encryptData(data: any): string {
-    const stringifiedData = JSON.stringify(data);
-    return btoa(stringifiedData);
-  }
-  
-  export function decryptData(encryptedData: string): any {
-    const decrypted = atob(encryptedData);
-    return JSON.parse(decrypted);
-  }
-  
+import { TaskType } from "@/@types/TaskType";
+
+export function encryptData(data: TaskType[]): string {
+  const stringifiedData = JSON.stringify(data);
+  return btoa(stringifiedData);
+}
+
+export function decryptData(encryptedData: string): TaskType[] {
+  const decrypted = atob(encryptedData);
+  return JSON.parse(decrypted) as TaskType[];
+}
