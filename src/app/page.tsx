252d6125e22a368f4b531fd4d87875ad5725280b
LuyandraBranco <luyandrabranco@gmail.com>
2024-09-21 16:32:25 +0100

    refactor: Tasklist removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import { Navbar } from "@/components/Navbar";
 import styles from "../styles/page.module.scss";
-import { TaskList } from "@/components/TaskList";
+
 
 export default function Home() {
   return (
     <div className={styles["home-container"]}>
       <Navbar />
       <main className={styles["tasklist-container"]}>
-        <TaskList />
+       
       </main>
     </div>
   );
